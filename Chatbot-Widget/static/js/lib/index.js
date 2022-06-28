ffffe2f3ffa7a2b364b97beaebd7645e759774d1
sabihkhan007 <47784793+sabihkhan007@users.noreply.github.com>
2022-06-28 12:48:34 +0200

    Update index.js

--- Chatbot-Widget/static/js/lib/index.js
+++ Chatbot-Widget/static/js/lib/index.js
@@ -3,7 +3,7 @@
 function include(file) {
     const script = document.createElement('script');
     script.src = file;
-    script.type = 'text/javascript';
+    script.type = 'application/json';
     script.defer = true;
 
     document.getElementsByTagName('head').item(0).appendChild(script);
